2f002664324cef7177d912fc541f4cadc3e3ca0e
Nurbek Nurjanov <nurbek.nurjanov@mail.ru>
2024-07-26 16:52:41 +0600

    update

--- src/helpers/response.ts
+++ src/helpers/response.ts
@@ -1,7 +1,9 @@
 import { Response } from 'express';
 
 export const handleResponseError = (res: Response, error: Error) =>
-  res.status(400).send(error.message);
+  res.status(400).send({
+    message: error.message,
+  });
 export const handleResponseFieldsError = (
   res: Response,
   fieldsErrors: Record<string, string>
